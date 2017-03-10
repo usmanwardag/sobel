3502425036db12de82d80f1d9fb90904fb56fd75
Usman <usmanwardag@gmail.com>
2017-03-10 21:50:24 +0500

    Added mac module

---
+++ source/mac.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+module mac(
+    input [7:0] in,
+    input [7:0] w,
+    input [7:0] b,
+    output [15:0] out
+    );
+
+wire [15:0] d;
+
+assign d = w * in;
+
+assign out = d + b;
+ 
+endmodule
